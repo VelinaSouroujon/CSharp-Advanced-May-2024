8ba0aa7101839b8873aad1f2319dc5df26401b15
VelinaSouroujon <velinasouroujon@gmail.com>
2024-06-17 23:13:10 +0300

    Add problem solution

---
+++ Functional Programming - Lab/1.SortEvenNumbers/1.SortEvenNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _1.SortEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int> parseToInt = str => int.Parse(str);
+            Func<int, bool> isEven = num => num % 2 == 0;
+            Func<int, int> sort = num => num;
+
+            int[] nums = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(parseToInt)
+                .Where(isEven)
+                .OrderBy(sort)
+                .ToArray();
+
+            Console.WriteLine(string.Join(", ", nums));
+        }
+    }
+}
