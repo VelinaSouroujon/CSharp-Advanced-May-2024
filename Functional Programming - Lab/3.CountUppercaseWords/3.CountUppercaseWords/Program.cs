92db0b08eb1aa1cc6c990800810103ce273c9f49
VelinaSouroujon <velinasouroujon@gmail.com>
2024-06-17 23:31:57 +0300

    Problem solution added

---
+++ Functional Programming - Lab/3.CountUppercaseWords/3.CountUppercaseWords/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _3.CountUppercaseWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<string> startsWithUppercase = word => Char.IsUpper(word[0]);
+
+            string[] words = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Where(x => startsWithUppercase(x))
+                .ToArray();
+
+            Console.WriteLine(string.Join(Environment.NewLine, words));
+        }
+    }
+}
