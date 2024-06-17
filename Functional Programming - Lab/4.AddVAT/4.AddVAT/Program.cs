090fde1d37703fb7ab0bf8bd192ee2fe94818f98
VelinaSouroujon <velinasouroujon@gmail.com>
2024-06-18 00:00:52 +0300

    Problem solution added

---
+++ Functional Programming - Lab/4.AddVAT/4.AddVAT/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _4.AddVAT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, double> parseToDouble = str => double.Parse(str);
+            Func<double, double> addVAT = num => 1.2 * num;
+            Func<double, string> roundNum = num => num.ToString("f2");
+
+            double[] numsWithVAT = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(parseToDouble)
+                .Select(x => addVAT(x))
+                .ToArray();
+
+            Console.WriteLine(string.Join(Environment.NewLine, numsWithVAT.Select(roundNum)));
+        }
+    }
+}
