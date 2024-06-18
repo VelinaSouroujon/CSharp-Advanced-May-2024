e6b3f255684d557913b6f05a4969a2dcee15fba4
VelinaSouroujon <velinasouroujon@gmail.com>
2024-06-19 02:41:14 +0300

    Problem solution added

---
+++ Stacks and Queues - Exercise/7.TruckTour/7.TruckTour/PetrolPump.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.TruckTour
+{
+    internal class PetrolPump
+    {
+        public PetrolPump(int petrolAmount, int distanceToTheNextPump)
+        {
+            this.PetrolAmount = petrolAmount;
+            this.DistanceToTheNextPump = distanceToTheNextPump;
+        }
+        public int PetrolAmount { get; set; }
+        public int DistanceToTheNextPump { get; set; }
+    }
+}
---
+++ Stacks and Queues - Exercise/7.TruckTour/7.TruckTour/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _7.TruckTour
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<PetrolPump> pumps = new Queue<PetrolPump>();
+            int numberOfPetrolPumps = int.Parse(Console.ReadLine());
+            int currentPetrolAmount = 0;
+            int successfulRoutePumpIdx = 0;
+            int currentIdx = 0;
+
+            for (int i = 0; i < numberOfPetrolPumps; i++)
+            {
+                int[] inputTokens = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                int petrolAmount = inputTokens[0];
+                int distanceToTheNextPump = inputTokens[1];
+
+                PetrolPump petrolPump = new PetrolPump(petrolAmount, distanceToTheNextPump);
+                pumps.Enqueue(petrolPump);
+            }
+
+            while (currentIdx < pumps.Count)
+            {
+                PetrolPump petrolPump = pumps.Dequeue();
+                pumps.Enqueue(petrolPump);
+                currentIdx++;
+                currentPetrolAmount += petrolPump.PetrolAmount;
+                
+                if(currentPetrolAmount < petrolPump.DistanceToTheNextPump)
+                {
+                    successfulRoutePumpIdx = currentIdx;
+                    currentPetrolAmount = 0;
+                }
+                else
+                {
+                    currentPetrolAmount -= petrolPump.DistanceToTheNextPump;
+                }
+            }
+            Console.WriteLine(successfulRoutePumpIdx);
+        }
+    }
+}
