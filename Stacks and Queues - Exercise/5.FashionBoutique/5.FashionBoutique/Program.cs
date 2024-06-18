08ec0b08a9bbf5ee0dd4802f3b3044c4612fbd4d
VelinaSouroujon <velinasouroujon@gmail.com>
2024-06-19 01:43:19 +0300

    Problem solution added

---
+++ Stacks and Queues - Exercise/5.FashionBoutique/5.FashionBoutique/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5.FashionBoutique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> clothes = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+            int initialRackCapacity = int.Parse(Console.ReadLine());
+            int currentRackCapacity = initialRackCapacity;
+            int racksCount = 1;
+
+            while(clothes.Any())
+            {
+                int currentCloth = clothes.Pop();
+                if(currentRackCapacity >= currentCloth)
+                {
+                    currentRackCapacity -= currentCloth;
+                }
+                else
+                {
+                    racksCount++;
+                    currentRackCapacity = initialRackCapacity - currentCloth;
+                }
+            }
+            Console.WriteLine(racksCount);
+        }
+    }
+}
