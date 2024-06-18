200a71ab248e139dec139eabb2b9898643854a16
VelinaSouroujon <velinasouroujon@gmail.com>
2024-06-18 17:37:15 +0300

    Problem solution added

---
+++ Functional Programming - Exercise/3.CustomMinFunction/3.CustomMinFunction/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+namespace _3.CustomMinFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<IEnumerable<int>, int> minNumber = nums => nums.Min();
+
+            int[] inputNums = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(minNumber(inputNums));
+        }
+    }
+}
