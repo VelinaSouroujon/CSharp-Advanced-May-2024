f865a363de90c64b1cb9160930a0b7af0f2ecb08
VelinaSouroujon <velinasouroujon@gmail.com>
2024-06-18 19:49:44 +0300

    Problem solution added

---
+++ Functional Programming - Exercise/6.ReverseАndExclude/6.ReverseАndExclude/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+namespace _6.ReverseАndExclude
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, bool> isDivisible = (num, n) => num % n == 0;
+
+            var nums = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            int n = int.Parse(Console.ReadLine());
+
+            nums.RemoveAll(x => isDivisible(x, n));
+            nums.Reverse();
+
+            Console.WriteLine(string.Join(" ", nums));
+        }
+    }
+}
