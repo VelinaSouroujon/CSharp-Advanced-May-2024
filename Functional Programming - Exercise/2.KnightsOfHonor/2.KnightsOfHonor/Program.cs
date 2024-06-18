eae2bb1b58a94017028efa07332898210a996540
VelinaSouroujon <velinasouroujon@gmail.com>
2024-06-18 17:25:08 +0300

    Problem solution added

---
+++ Functional Programming - Exercise/2.KnightsOfHonor/2.KnightsOfHonor/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace _2.KnightsOfHonor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, string> nameWithSir = name => $"Sir {name}";
+            Action <string[]> printAllNames = names => 
+                Console.WriteLine(string.Join(Environment.NewLine, names.Select(n => nameWithSir(n))));
+
+            string[] inputNames = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            printAllNames(inputNames);
+        }
+    }
+}
