90ca12f8902ea904147a0f741f04df96b93e29f0
VelinaSouroujon <velinasouroujon@gmail.com>
2024-06-18 20:17:33 +0300

    Problem solution added

---
+++ Functional Programming - Exercise/8. ListOfPredicates/8. ListOfPredicates/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _8._ListOfPredicates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            HashSet<int> dividers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToHashSet();
+
+            Func<IEnumerable<int>, int, bool> filter = (collection, num) =>
+                collection.All(div => num % div == 0);
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (filter(dividers, i))
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
