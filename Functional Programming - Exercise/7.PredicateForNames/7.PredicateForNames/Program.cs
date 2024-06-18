90e59d43ff6c1a1088d5ef6ff77b748644419ba5
VelinaSouroujon <velinasouroujon@gmail.com>
2024-06-18 20:01:39 +0300

    Problem solution added

---
+++ Functional Programming - Exercise/7.PredicateForNames/7.PredicateForNames/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _7.PredicateForNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Func<string, int, bool> filter = (name, maxLength) => name.Length <= maxLength;
+
+            string[] names = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Where(name => filter(name, n))
+                .ToArray();
+
+            Console.WriteLine(string.Join(Environment.NewLine, names));
+        }
+    }
+}
