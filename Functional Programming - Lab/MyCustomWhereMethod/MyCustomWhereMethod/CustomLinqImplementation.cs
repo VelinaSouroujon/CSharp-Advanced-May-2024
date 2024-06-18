dcdd5a5b3c5139f01358110635ef56052ffa4efc
VelinaSouroujon <velinasouroujon@gmail.com>
2024-06-18 16:17:58 +0300

    Added solution for custom where method implementation

---
+++ Functional Programming - Lab/MyCustomWhereMethod/MyCustomWhereMethod/CustomLinqImplementation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCustomWhereMethod
+{
+    internal static class CustomLinqImplementation
+    {
+        public static IEnumerable<int> Where(this IEnumerable<int> collection, Func<int, bool> filter)
+        {
+            List<int> items = new List<int>();
+
+            foreach (int item in collection)
+            {
+                if (filter(item))
+                {
+                    items.Add(item);
+                }
+            }
+
+            return items;
+        }
+
+        public static List<int> ToList(this IEnumerable<int> collection)
+        {
+            List<int> items = new List<int>();
+
+            foreach (int item in collection)
+            {
+                items.Add(item);
+            }
+
+            return items;
+        }
+    }
+}
---
+++ Functional Programming - Lab/MyCustomWhereMethod/MyCustomWhereMethod/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyCustomWhereMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = new() { 1, 2, 3, 4, 5, 6 };
+
+            nums = nums.Where(n => n % 2 == 0).ToList();
+
+            Console.WriteLine(string.Join(" ", nums));
+        }
+    }
+}
