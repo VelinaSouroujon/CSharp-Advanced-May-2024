5b9c027e38db22a7a2a4d035cbb24138cc7d082a
VelinaSouroujon <velinasouroujon@gmail.com>
2024-06-18 17:07:52 +0300

    Problem solution added

---
+++ Functional Programming - Exercise/1.ActionPrint/1.ActionPrint/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _1.ActionPrint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string[]> print = names => Console.WriteLine(string.Join(Environment.NewLine, names));
+            string[] words = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            print(words);
+        }
+    }
+}
