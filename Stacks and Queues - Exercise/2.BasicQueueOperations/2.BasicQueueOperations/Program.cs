a54a8a5756e89c15bccb0bc16fcfeac4d1a49b66
VelinaSouroujon <velinasouroujon@gmail.com>
2024-06-18 17:09:24 +0300

    Problem solution added

---
+++ Stacks and Queues - Exercise/2.BasicQueueOperations/2.BasicQueueOperations/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2.BasicQueueOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+            int[] input = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+            int enqueueCount = input[0];
+            int dequeueCount = input[1];
+            int searchedNumber = input[2];
+
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int i = 0; i < enqueueCount; i++)
+            {
+                queue.Enqueue(numbers[i]);
+            }
+            for (int i = 0;i < dequeueCount; i++)
+            {
+                if(queue.Any())
+                {
+                    queue.Dequeue();
+                }
+                else
+                {
+                    break;
+                }
+            }
+            if(queue.Contains(searchedNumber))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                if(queue.Any())
+                {
+                    Console.WriteLine(queue.Min());
+                }
+                else
+                {
+                    Console.WriteLine(0);
+                }
+            }
+        }
+    }
+}
