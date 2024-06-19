9811a45ce573c99b75cf10446804ae93bd64fb34
VelinaSouroujon <velinasouroujon@gmail.com>
2024-06-19 21:40:55 +0300

    Problem solution added

---
+++ Functional Programming - Exercise/11.TriFunction/11.TriFunction/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _11.TriFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] inputNames = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Func<string, int, bool> filter = (name, n) => name.Sum(x => x) >= n;
+
+            Func<IEnumerable<string>, Func<string, int, bool>, int, string> findName = (collection, filter, n) =>
+                collection.First(x => filter(x, n));
+
+            Console.WriteLine(findName(inputNames, filter, n));
+        }
+    }
+}
