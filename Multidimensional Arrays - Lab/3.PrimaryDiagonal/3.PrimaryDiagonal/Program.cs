dfb587cbaa91810565807955d5bd459bfe98ff50
VelinaSouroujon <velinasouroujon@gmail.com>
2024-06-20 01:55:11 +0300

    Problem solution added

---
+++ Multidimensional Arrays - Lab/3.PrimaryDiagonal/3.PrimaryDiagonal/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _3.PrimaryDiagonal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+            int primaryDiagonalSum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] rowElements = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int j = 0; j < rowElements.Length; j++)
+                {
+                    int element = rowElements[j];
+                    matrix[i, j] = element;
+
+                    if(i == j)
+                    {
+                        primaryDiagonalSum += element;
+                    }
+                }
+            }
+            Console.WriteLine(primaryDiagonalSum);
+        }
+    }
+}
