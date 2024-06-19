f4fd865efb5b69d9ba32590caab458208bb01c88
VelinaSouroujon <velinasouroujon@gmail.com>
2024-06-19 13:29:09 +0300

    Problem solution added

---
+++ Stacks and Queues - Exercise/8.BalancedParentheses/8.BalancedParentheses/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _8.BalancedParentheses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> parentheses = new Stack<char>();
+            string expression = Console.ReadLine();
+            bool isBalanced = true;
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                char c = expression[i];
+
+                if (IsOpenParenthesis(c))
+                {
+                    parentheses.Push(c);
+                }
+                else if (IsClosingParenthesis(c))
+                {
+                    if((parentheses.Count == 0) || (parentheses.Peek() != MatchingParenthesis(c)))
+                    {
+                        isBalanced = false;
+                        break;
+                    }
+                    
+                    parentheses.Pop();
+                }
+            }
+            if (parentheses.Count != 0)
+            {
+                isBalanced = false;
+            }
+
+            if(isBalanced)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+        static bool IsOpenParenthesis(char p)
+        {
+            return p == '(' || p == '[' || p == '{';
+        }
+        static bool IsClosingParenthesis(char p)
+        {
+            return p == ')' || p == ']' || p == '}';
+        }
+        static char MatchingParenthesis(char p)
+        {
+            if (p == ')')
+            {
+                return '(';
+            }
+            else if (p == ']')
+            {
+                return '[';
+            }
+            else if (p == '}')
+            {
+                return '{';
+            }
+            return '\0';
+        }
+    }
+}
