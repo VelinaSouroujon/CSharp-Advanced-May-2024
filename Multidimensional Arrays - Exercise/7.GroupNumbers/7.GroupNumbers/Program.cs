d91d0af8d14c7b993ac3df8986164829fcbb401f
VelinaSouroujon <velinasouroujon@gmail.com>
2024-06-21 01:01:32 +0300

    Problem solution added

---
+++ Multidimensional Arrays - Exercise/7.GroupNumbers/7.GroupNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _7.GroupNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var numsGroups = numbers.GroupBy(x => Math.Abs(x % 3)).OrderBy(x => x.Key);
+
+            foreach ( var group in numsGroups )
+            {
+                foreach(int numInGroup in group)
+                {
+                    Console.Write(numInGroup + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
