d6c8d94dfc50f93ef2406b417f5c7896f3636873
VelinaSouroujon <velinasouroujon@gmail.com>
2024-06-20 14:00:05 +0300

    Problem solution added

---
+++ Multidimensional Arrays - Lab/4.SymbolInMatrix/4.SymbolInMatrix/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace _4.SymbolInMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string rowElements = Console.ReadLine();
+
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = rowElements[j];
+                }
+            }
+            char searchedSymbol = char.Parse(Console.ReadLine());
+            bool symbolFound = false;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] == searchedSymbol)
+                    {
+                        symbolFound = true;
+                        Console.WriteLine($"({i}, {j})");
+                        break;
+                    }
+                }
+                if(symbolFound)
+                {
+                    break;
+                }
+            }
+            if (!symbolFound)
+            {
+                Console.WriteLine($"{searchedSymbol} does not occur in the matrix");
+            }
+        }
+    }
+}
