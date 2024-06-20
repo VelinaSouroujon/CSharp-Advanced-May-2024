b31fb04f49ba486a867ba4b06956003c19441fbd
VelinaSouroujon <velinasouroujon@gmail.com>
2024-06-20 16:07:57 +0300

    Problem solution added

---
+++ Multidimensional Arrays - Exercise/1.DiagonalDifference/1.DiagonalDifference/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace _1.DiagonalDifference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+            int primaryDiagonalSum = 0;
+            int secondaryDiagonalSum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] rowElements = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int j = 0; j < rowElements.Length; j++)
+                {
+                    int element = rowElements[j];
+                    matrix[i, j] = element;
+
+                    if(i == j)
+                    {
+                        primaryDiagonalSum += element;
+                    }
+                    if(i + j == n - 1)
+                    {
+                        secondaryDiagonalSum += element;
+                    }
+                }
+            }
+            Console.WriteLine(Math.Abs(primaryDiagonalSum - secondaryDiagonalSum));
+        }
+    }
+}
