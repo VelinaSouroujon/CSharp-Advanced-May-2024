5aa361fef4d7790cad0793fcfbdc77c4115fd3b4
VelinaSouroujon <velinasouroujon@gmail.com>
2024-06-21 22:52:26 +0300

    Problem solution added

---
+++ Defining Classes - Exercise/6.SpeedRacing/6.SpeedRacing/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace _6.SpeedRacing
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Car[] cars = new Car[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] inputTokens = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string model = inputTokens[0];
+                double fuelAmount = double.Parse(inputTokens[1]);
+                double fuelConsumptionFor1km = double.Parse(inputTokens[2]);
+
+                Car car = new Car(model, fuelAmount, fuelConsumptionFor1km);
+                cars[i] = car;
+            }
+
+            string input = "";
+            while((input = Console.ReadLine()).ToLower() != "end")
+            {
+                string[] tokens = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string model = tokens[1];
+                double distance = double.Parse(tokens[2]);
+
+                Car carToDrive = cars.FirstOrDefault(c => c.Model == model);
+                carToDrive.Drive(distance);
+            }
+
+            foreach (Car car in cars)
+            {
+                Console.WriteLine(car);
+            }
+        }
+    }
+}
