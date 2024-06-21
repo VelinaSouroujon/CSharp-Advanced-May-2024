79f0a4b23c8dd853014b193fe8f9c15485f914ef
VelinaSouroujon <velinasouroujon@gmail.com>
2024-06-21 22:02:23 +0300

    Added constructors to class Person

--- Defining Classes - Exercise/1.DefineClassPerson/1.DefineClassPerson/Person.cs
+++ Defining Classes - Exercise/1.DefineClassPerson/1.DefineClassPerson/Person.cs
@@ -11,6 +11,22 @@ namespace DefiningClasses
         private string name;
         private int age;
 
+        public Person()
+        {
+            Name = "No name";
+            Age = 1;
+        }
+        public Person(int age)
+            : this()
+        {
+            Age = age;
+        }
+        public Person(string name, int age)
+            : this(age)
+        {
+            Name = name;
+        }
+
         public string Name
         {
             get
