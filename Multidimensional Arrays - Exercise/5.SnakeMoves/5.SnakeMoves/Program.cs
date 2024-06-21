dbbebf7ade65b499456cb602b9fa4a36610b72e3
VelinaSouroujon <velinasouroujon@gmail.com>
2024-06-22 02:00:04 +0300

    Problem solution added

---
+++ Multidimensional Arrays - Exercise/5.SnakeMoves/5.SnakeMoves/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+
+namespace _5.SnakeMoves
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] rowsColsCount = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            int rows = int.Parse(rowsColsCount[0]);
+            int cols = int.Parse(rowsColsCount[1]);
+            char[,] matrix = new char[rows, cols];
+            string snake = Console.ReadLine();
+            int idxSnake = 0;
+            bool fromLeftToRight = true;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (fromLeftToRight)
+                    {
+                        matrix[i, j] = snake[idxSnake];
+                    }
+                    else
+                    {
+                        matrix[i, cols - 1 - j] = snake[idxSnake];
+                    }
+
+                    idxSnake++;
+                    if(idxSnake == snake.Length)
+                    {
+                        idxSnake = 0;
+                    }
+                    if(j == cols - 1)
+                    {
+                        if(fromLeftToRight)
+                        {
+                            fromLeftToRight = false;
+                        }
+                        else
+                        {
+                            fromLeftToRight = true;
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < rows; i ++)
+            {
+                for(int j = 0;j < cols; j ++)
+                {
+                    Console.Write(matrix[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
