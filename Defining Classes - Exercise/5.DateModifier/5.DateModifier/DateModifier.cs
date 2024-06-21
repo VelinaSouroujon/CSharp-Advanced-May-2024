5b6b89437102937f91ded1bca84ab383a9216fb6
VelinaSouroujon <velinasouroujon@gmail.com>
2024-06-21 22:29:05 +0300

    Problem solution added

---
+++ Defining Classes - Exercise/5.DateModifier/5.DateModifier/DateModifier.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.DateModifier
+{
+    public static class DateModifier
+    {
+        public static int DaysDifference(string inputDate1, string inputDate2)
+        {
+            DateTime date1 = GetDate(inputDate1);
+            DateTime date2 = GetDate(inputDate2);
+
+            return Math.Abs((date2 - date1).Days);
+        }
+        private static DateTime GetDate(string date)
+        {
+            int[] dateInfo = date
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int year = dateInfo[0];
+            int month = dateInfo[1];
+            int day = dateInfo[2];
+
+            return new DateTime(year, month, day);
+        }
+    }
+}
---
+++ Defining Classes - Exercise/5.DateModifier/5.DateModifier/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace _5.DateModifier
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string date1 = Console.ReadLine();
+            string date2 = Console.ReadLine();
+
+            Console.WriteLine(DateModifier.DaysDifference(date1, date2));
+        }
+    }
+}
