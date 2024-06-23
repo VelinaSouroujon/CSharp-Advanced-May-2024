7d092181572f3e91f06f85dc80ac6a472bbed841
VelinaSouroujon <velinasouroujon@gmail.com>
2024-06-23 21:31:00 +0300

    Workshop: Implementing Stack

---
+++ Implementing List, Stack and Queue/02.CustomStack/02.CustomStack/CustomStack.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.CustomStack
+{
+    public class CustomStack
+    {
+        private const int Capacity = 4;
+
+        private int[] items;
+        private int counter;
+
+        public CustomStack()
+        {
+            items = new int[Capacity];
+        }
+
+        public int Count => counter;
+        public void Push(int value)
+        {
+            if(counter == items.Length)
+            {
+                int[] tempArray = new int[items.Length * 2];
+                Array.Copy(items, tempArray, items.Length);
+                items = tempArray;
+            }
+
+            items[counter++] = value;
+        }
+
+        public int Pop()
+        {
+            if(counter == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            int removedElement = items[counter - 1];
+            items[counter - 1] = default;
+
+            counter--;
+
+            if(items.Length / 2 >= counter)
+            {
+                int[] tempArray = new int[items.Length / 2];
+                Array.Copy(items, tempArray, tempArray.Length);
+                items = tempArray;
+            }
+
+            return removedElement;
+        }
+
+        public int Peek()
+        {
+            if (counter == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return items[counter - 1];
+        }
+
+        public void ForEach(Action<int> action)
+        {
+            for(int i = counter - 1; i >= 0; i--)
+            {
+                action(items[i]);
+            }
+        }
+    }
+}
---
+++ Implementing List, Stack and Queue/02.CustomStack/02.CustomStack/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _02.CustomStack
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomStack stack = new CustomStack();
+
+            stack.Push(10);
+            stack.Push(20);
+            stack.Push(30);
+            stack.Push(40);
+            stack.Push(50);
+
+            Console.WriteLine(stack.Peek());
+
+            //while (stack.Count > 0)
+            //{
+            //    Console.WriteLine(stack.Pop());
+            //}
+
+            stack.ForEach(Console.WriteLine);
+        }
+    }
+}
