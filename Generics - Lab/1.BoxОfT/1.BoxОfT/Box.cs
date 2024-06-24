caff171db289f9930b4a3af90852d528314c26f7
VelinaSouroujon <velinasouroujon@gmail.com>
2024-06-24 18:05:18 +0300

    Problem solution added

---
+++ Generics - Lab/1.BoxОfT/1.BoxОfT/Box.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private Stack<T> items;
+
+        public Box()
+        {
+            items = new Stack<T>();
+        }
+        public int Count => items.Count;
+        public void Add(T element)
+        {
+            items.Push(element);
+        }
+        public T Remove()
+        {
+            return items.Pop();
+        }
+    }
+}
