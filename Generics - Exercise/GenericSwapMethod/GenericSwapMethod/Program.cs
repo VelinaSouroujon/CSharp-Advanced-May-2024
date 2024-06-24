17432141fcc63800d8ba13edc931c312450349c3
VelinaSouroujon <velinasouroujon@gmail.com>
2024-06-25 01:15:45 +0300

    Testing generic method with integers

---
+++ Generics - Exercise/GenericSwapMethod/GenericSwapMethod/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericSwapMethod
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<int> list = new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                list.Add(num);
+            }
+            string[] inputIndexesToSwap = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            int firstIndex = int.Parse(inputIndexesToSwap[0]);
+            int secondIndex = int.Parse(inputIndexesToSwap[1]);
+            Swap(list, firstIndex, secondIndex);
+
+            foreach(int item in list)
+            {
+                Console.WriteLine($"{item.GetType()}: {item}");
+            }
+        }
+        static void Swap<T>(List<T> list, int firstIndex, int secondIndex)
+        {
+            T temp = list[firstIndex];
+            list[firstIndex] = list[secondIndex];
+            list[secondIndex] = temp;
+        }
+    }
+}
