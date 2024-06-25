255f4e03a1caa4717ad6471f7a3f124485b32438
VelinaSouroujon <velinasouroujon@gmail.com>
2024-06-25 14:50:37 +0300

    Problem solution added

---
+++ Generics - Exercise/Tuple/Tuple/MyTuple.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tuple
+{
+    public class MyTuple<T, U>
+    {
+        public MyTuple(T item1, U item2)
+        {
+            Item1 = item1;
+            Item2 = item2;
+        }
+
+        public T Item1 { get; set; }
+        public U Item2 { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Item1} -> {Item2}";
+        }
+    }
+}
---
+++ Generics - Exercise/Tuple/Tuple/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Tuple
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            string fullName = input[0] + " " + input[1];
+            string address = input[2];
+            MyTuple<string, string> personNameAddress = new MyTuple<string, string>(fullName, address);
+            Console.WriteLine(personNameAddress);
+
+            input = Console.ReadLine().Split();
+            string name = input[0];
+            int amountOfBeer = int.Parse(input[1]);
+            MyTuple<string, int> personNameAmountOfBeer = new MyTuple<string, int>(name, amountOfBeer);
+            Console.WriteLine(personNameAmountOfBeer);
+
+            input = Console.ReadLine().Split();
+            MyTuple<int, double> numbers = new MyTuple<int, double>(int.Parse(input[0]), double.Parse(input[1]));
+            Console.WriteLine(numbers);
+        }
+    }
+}
