66d3dff0e017f3d7eb970367b593ed6f99d43470
VelinaSouroujon <velinasouroujon@gmail.com>
2024-06-26 13:18:19 +0300

    Problem solution added

---
+++ Sets and Dictionaries Advanced - Lab/3.LargestThreeNumbers/3.LargestThreeNumbers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace _3.LargestThreeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var largestThreeNums = Console.ReadLine().Split().Select(double.Parse).OrderByDescending(x => x).Take(3);
+            Console.WriteLine(string.Join(" ", largestThreeNums));
+        }
+    }
+}
