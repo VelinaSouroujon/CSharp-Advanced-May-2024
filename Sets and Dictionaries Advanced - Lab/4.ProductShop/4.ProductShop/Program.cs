2d6d33c8ec26b1bce198e10944cc8d8bb9c9cf39
VelinaSouroujon <velinasouroujon@gmail.com>
2024-06-26 15:03:02 +0300

    Problem solution added

---
+++ Sets and Dictionaries Advanced - Lab/4.ProductShop/4.ProductShop/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4.ProductShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<string, Dictionary<string, double>> shops = new SortedDictionary<string, Dictionary<string, double>>();
+            string input = "";
+            while((input = Console.ReadLine()).ToLower() != "revision")
+            {
+                string[] info = input.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+                string shop = info[0];
+                string product = info[1];
+                double price = double.Parse(info[2]);
+
+                if(!shops.ContainsKey(shop))
+                {
+                    shops.Add(shop, new Dictionary<string, double>());
+                }
+                shops[shop].Add(product, price);
+            }
+
+            foreach(var kvp in shops)
+            {
+                Console.WriteLine($"{kvp.Key}->");
+
+                foreach(var kvp2 in kvp.Value)
+                {
+                    Console.WriteLine($"Product: {kvp2.Key}, Price: {kvp2.Value}");
+                }
+            }
+        }
+    }
+}
