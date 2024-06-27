7d3d9540006455be5e64600c46e52038b6036314
VelinaSouroujon <velinasouroujon@gmail.com>
2024-06-27 18:42:24 +0300

    Problem solution added

---
+++ Sets and Dictionaries Advanced - Lab/7.ParkingLot/7.ParkingLot/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _7.ParkingLot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> carNumbers = new HashSet<string>();
+            string input = "";
+
+            while((input = Console.ReadLine()).ToLower() != "end")
+            {
+                string[] tokens = input.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+                string command = tokens[0].ToLower();
+                string carNum = tokens[1];
+
+                if(command == "in")
+                {
+                    carNumbers.Add(carNum);
+                }
+                else if(command == "out") 
+                {
+                    carNumbers.Remove(carNum);
+                }
+            }
+
+            if (carNumbers.Count > 0)
+            {
+                foreach (string carNumber in carNumbers)
+                {
+                    Console.WriteLine(carNumber);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+    }
+}
