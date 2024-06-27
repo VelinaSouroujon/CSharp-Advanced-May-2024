d5093bca4171790a4faa7735a59cd62d52e17f95
VelinaSouroujon <velinasouroujon@gmail.com>
2024-06-27 17:44:04 +0300

    Problem solution added

---
+++ Sets and Dictionaries Advanced - Lab/5.CitiesByContinentAndCountry/5.CitiesByContinentAndCountry/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5.CitiesByContinentAndCountry
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, List<string>>> continentsInfo = new Dictionary<string, Dictionary<string, List<string>>>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] inputTokens = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string continent = inputTokens[0];
+                string country = inputTokens[1];
+                string city = inputTokens[2];
+
+                if(!continentsInfo.ContainsKey(continent))
+                {
+                    continentsInfo[continent] = new Dictionary<string, List<string>>();
+                }
+                if (!continentsInfo[continent].ContainsKey(country))
+                {
+                    continentsInfo[continent][country] = new List<string>();
+                }
+                continentsInfo[continent][country].Add(city);
+            }
+
+            foreach(var kvp in continentsInfo)
+            {
+                Console.WriteLine($"{kvp.Key}:");
+                
+                foreach(var kvp2 in kvp.Value)
+                {
+                    Console.WriteLine($"{kvp2.Key} -> {string.Join(", ", kvp2.Value)}");
+                }
+            }
+        }
+    }
+}
