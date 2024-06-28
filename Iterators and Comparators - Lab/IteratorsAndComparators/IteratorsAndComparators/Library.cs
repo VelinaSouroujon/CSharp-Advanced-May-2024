22e5612a0aa7b7440885384cc7a43f025ca8964e
VelinaSouroujon <velinasouroujon@gmail.com>
2024-06-28 21:03:47 +0300

    Created nested LibraryIterator class that implements IEnumerator<Book> and using foreach to iterate through books

--- Iterators and Comparators - Lab/IteratorsAndComparators/IteratorsAndComparators/Library.cs
+++ Iterators and Comparators - Lab/IteratorsAndComparators/IteratorsAndComparators/Library.cs
@@ -17,12 +17,43 @@ namespace IteratorsAndComparators
         }
         public IEnumerator<Book> GetEnumerator()
         {
-            throw new NotImplementedException();
+            return new LibraryIterator(books);
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
+        }
+
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            private int currentIndex;
+            private readonly List<Book> books;
+
+            public LibraryIterator(IEnumerable<Book> books)
+            {
+                Reset();
+                this.books = books.ToList();
+            }
+            public Book Current => books[currentIndex];
+
+            object IEnumerator.Current => Current;
+
+            public bool MoveNext()
+            {
+                currentIndex++;
+
+                return currentIndex < books.Count;
+            }
+
+            public void Reset()
+            {
+                currentIndex = -1;
+            }
+            public void Dispose()
+            {
+
+            }
         }
     }
 }
