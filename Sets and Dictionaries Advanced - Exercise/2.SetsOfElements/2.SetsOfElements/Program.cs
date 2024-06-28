dfee942b0f6f5c9c21206773dedc3d35c0f3b504
VelinaSouroujon <velinasouroujon@gmail.com>
2024-06-29 02:29:07 +0300

    Problem solution added

---
+++ Sets and Dictionaries Advanced - Exercise/2.SetsOfElements/2.SetsOfElements/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2.SetsOfElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> firstSetOfNums = new HashSet<int>();
+            HashSet<int> secondSetOfNums = new HashSet<int>();
+
+            string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            int n = int.Parse(input[0]);
+            int m = int.Parse(input[1]);
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                firstSetOfNums.Add(number);
+            }
+            for (int i = 0;i < m; i++)
+            {
+                int number = int.Parse((Console.ReadLine()));
+                secondSetOfNums.Add(number);
+            }
+            firstSetOfNums.IntersectWith(secondSetOfNums);
+
+            Console.WriteLine(string.Join(" ", firstSetOfNums));
+        }
+    }
+}
