6bc4213d2dadb24bad967446493bb3115ffbb2e5
VelinaSouroujon <velinasouroujon@gmail.com>
2024-06-30 02:21:07 +0300

    Problem solution added

---
+++ Sets and Dictionaries Advanced - Exercise/5.CountSymbols/5.CountSymbols/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _5.CountSymbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<char, int> charOccurrences = new SortedDictionary<char, int>();
+            string text = Console.ReadLine();
+
+            foreach(char c in text)
+            {
+                if(!charOccurrences.ContainsKey(c))
+                {
+                    charOccurrences.Add(c, 1);
+                }
+                else
+                {
+                    charOccurrences[c]++;
+                }
+            }
+
+            foreach(var kvp in charOccurrences)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value} time/s");
+            }
+        }
+    }
+}
