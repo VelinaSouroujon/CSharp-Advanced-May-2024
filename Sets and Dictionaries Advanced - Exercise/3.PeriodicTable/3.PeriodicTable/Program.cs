1b0935ccb44aed927fd90b4d7f07649222d1d067
VelinaSouroujon <velinasouroujon@gmail.com>
2024-06-29 14:17:35 +0300

    Problem solution added

---
+++ Sets and Dictionaries Advanced - Exercise/3.PeriodicTable/3.PeriodicTable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3.PeriodicTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            SortedSet<string> chemicalElements = new SortedSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] chemicalCompounds = Console.ReadLine().Split();
+                foreach(string c in chemicalCompounds)
+                {
+                    chemicalElements.Add(c);
+                }
+            }
+            Console.WriteLine(string.Join(" ", chemicalElements));
+        }
+    }
+}
