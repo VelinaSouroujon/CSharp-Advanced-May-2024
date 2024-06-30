ef8dbaf414690708588f1cda3503f1a15ebc667f
VelinaSouroujon <velinasouroujon@gmail.com>
2024-06-30 22:55:14 +0300

    Problem solution added

---
+++ Iterators and Comparators - Exercise/4.Froggy/4.Froggy/Lake.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Froggy
+{
+    public class Lake : IEnumerable<int>
+    {
+        private int[] items;
+        public Lake(IEnumerable<int> collection)
+        {
+            items = collection.ToArray();
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < items.Length; i += 2)
+            {
+                yield return items[i];
+            }
+
+            int startIdx = items.Length % 2 == 0 ? items.Length - 1 : items.Length - 2;
+            for (int i = startIdx; i >= 0; i -= 2)
+            {
+                yield return items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Iterators and Comparators - Exercise/4.Froggy/4.Froggy/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace _4.Froggy
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> inputNumbers = Console.ReadLine().Split(", ").Select(int.Parse);
+            Lake lake = new Lake(inputNumbers);
+
+            Console.WriteLine(string.Join(", ", lake));
+        }
+    }
+}
