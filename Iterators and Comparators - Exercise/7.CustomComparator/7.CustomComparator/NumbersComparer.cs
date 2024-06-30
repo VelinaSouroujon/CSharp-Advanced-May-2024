edf6e2dcd1eedc70de1ccb609d69a916bd72ed0f
VelinaSouroujon <velinasouroujon@gmail.com>
2024-07-01 00:00:06 +0300

    Problem solution added

---
+++ Iterators and Comparators - Exercise/7.CustomComparator/7.CustomComparator/NumbersComparer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.CustomComparator
+{
+    public class NumbersComparer : IComparer<int>
+    {
+        public int Compare(int x, int y)
+        {
+            int result;
+
+            if ((x % 2 == 0 && y % 2 == 0)
+                || (x % 2 != 0 && y % 2 != 0))
+            {
+                result = x.CompareTo(y);
+            }
+            else if(x % 2 == 0)
+            {
+                result = -1;
+            }
+            else
+            {
+                result = 1;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Iterators and Comparators - Exercise/7.CustomComparator/7.CustomComparator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _7.CustomComparator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            NumbersComparer comparer = new NumbersComparer();
+            Array.Sort(numbers, comparer);
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
