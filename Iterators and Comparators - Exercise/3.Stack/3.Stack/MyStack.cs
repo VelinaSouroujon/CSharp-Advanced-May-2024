52cc47ca37b404f4c11ca3a045f977afa85e7f30
VelinaSouroujon <velinasouroujon@gmail.com>
2024-06-30 21:29:52 +0300

    Problem solution added

---
+++ Iterators and Comparators - Exercise/3.Stack/3.Stack/MyStack.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Stack
+{
+    public class MyStack<T> : IEnumerable<T>
+    {
+        private const int InitialCapacity = 4;
+
+        private T[] items;
+        private int index;
+
+        public MyStack()
+        {
+            items = new T[InitialCapacity];
+        }
+        public int Count => index;
+        public void Push(T item)
+        {
+            if(!IsIndexValid())
+            {
+                Resize();
+            }
+
+            items[index] = item;
+            index++;
+        }
+        public T Pop()
+        {
+            if(Count == 0)
+            {
+                throw new InvalidOperationException("No elements");
+            }
+
+            index--;
+            return items[index];
+        }
+        private void Resize()
+        {
+            T[] temp = new T[2 * items.Length];
+            Array.Copy(items, temp, items.Length);
+            items = temp;
+        }
+        private bool IsIndexValid()
+        {
+            return index < items.Length;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for(int i = Count - 1; i >= 0; i--)
+            {
+                yield return items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Iterators and Comparators - Exercise/3.Stack/3.Stack/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace _3.Stack
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack<int> myStack = new MyStack<int>();
+
+            string input = "";
+            while((input = Console.ReadLine().ToLower()) != "end")
+            {
+                try
+                {
+                    string[] tokens = input
+                        .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    string command = tokens[0];
+                    if (command == "push")
+                    {
+                        int[] elementsToPush = tokens
+                            .Skip(1)
+                            .Select(int.Parse)
+                            .ToArray();
+
+                        foreach (int element in elementsToPush)
+                        {
+                            myStack.Push(element);
+                        }
+                    }
+
+                    else if (command == "pop")
+                    {
+                        myStack.Pop();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            } 
+            
+            PrintStack(myStack);
+            PrintStack(myStack);
+        }
+        static void PrintStack<T>(MyStack<T> myStack)
+        {
+            foreach(T element in myStack)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
