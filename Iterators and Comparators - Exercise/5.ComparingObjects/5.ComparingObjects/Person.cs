8f6f51187c2be31b6bbaeb1760c8f09f882e6d6a
VelinaSouroujon <velinasouroujon@gmail.com>
2024-06-30 23:17:03 +0300

    Problem solution added

---
+++ Iterators and Comparators - Exercise/5.ComparingObjects/5.ComparingObjects/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.ComparingObjects
+{
+    public class Person : IComparable<Person>
+    {
+        public Person(string name, int age, string town)
+        {
+            Name = name;
+            Age = age;
+            Town = town;
+        }
+
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+        public string Town { get; private set; }
+
+        public int CompareTo(Person other)
+        {
+            int result = Name.CompareTo(other.Name);
+
+            if (result == 0)
+            {
+                result = Age.CompareTo(other.Age);
+            }
+            if(result == 0)
+            {
+                result = Town.CompareTo(other.Town);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Iterators and Comparators - Exercise/5.ComparingObjects/5.ComparingObjects/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _5.ComparingObjects
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+
+            string input = "";
+            while((input = Console.ReadLine()).ToLower() != "end")
+            {
+                string[] personInfo = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string name = personInfo[0];
+                int age = int.Parse(personInfo[1]);
+                string town = personInfo[2];
+
+                people.Add(new Person(name, age, town));
+            }
+
+            int n = int.Parse(Console.ReadLine());
+            Person person = people[n - 1];
+
+            int matchingPeopleCount = people.Count(x => x.CompareTo(person) == 0);
+
+            int notMatchingPeopleCount = people.Count - matchingPeopleCount;
+
+            if(matchingPeopleCount == 1)
+            {
+                Console.WriteLine("No matches");
+            }
+            else
+            {
+                Console.WriteLine($"{matchingPeopleCount} {notMatchingPeopleCount} {people.Count}");
+            }
+        }
+    }
+}
