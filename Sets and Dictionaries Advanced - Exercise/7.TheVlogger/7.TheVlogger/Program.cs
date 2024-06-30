43b7ecdf9b5ff583ab4e3f74a8437bf9bf808b30
VelinaSouroujon <velinasouroujon@gmail.com>
2024-06-30 15:45:49 +0300

    Problem solution added

---
+++ Sets and Dictionaries Advanced - Exercise/7.TheVlogger/7.TheVlogger/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace _7.TheVlogger
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Vlogger> vloggersAndFollowers = new Dictionary<string, Vlogger>();
+            string input = "";
+            while ((input = Console.ReadLine()).ToLower() != "statistics")
+            {
+                string[] tokens = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string name = tokens[0];
+                string command = tokens[1];
+
+                if ((command == "joined") && (!vloggersAndFollowers.ContainsKey(name)))
+                {
+                    Vlogger vlogger = new Vlogger(name);
+                    vloggersAndFollowers[name] = vlogger;
+                }
+                else if (command == "followed")
+                {
+                    string followedVlogger = tokens[2];
+
+                    if (vloggersAndFollowers.ContainsKey(name)
+                        && vloggersAndFollowers.ContainsKey(followedVlogger)
+                        && (!vloggersAndFollowers[followedVlogger].followers.Contains(name))
+                        && (name != followedVlogger))
+                    {
+                        vloggersAndFollowers[followedVlogger].followers.Add(name);
+                        vloggersAndFollowers[name].followingCount++;
+                    }
+                }
+            }
+            vloggersAndFollowers = vloggersAndFollowers
+                .OrderByDescending(x => x.Value.followers.Count)
+                .ThenBy(x => x.Value.followingCount)
+                .ToDictionary(x => x.Key, x => x.Value);
+
+            Console.WriteLine($"The V-Logger has a total of {vloggersAndFollowers.Count} vloggers in its logs.");
+            int counter = 1;
+
+            foreach(var kvp in vloggersAndFollowers)
+            {
+                Vlogger vlogger = kvp.Value;
+                Console.WriteLine($"{counter}. {kvp.Key} : {vlogger.followers.Count} followers, {vlogger.followingCount} following");
+
+                if (counter == 1)
+                {
+                    foreach (string followerName in vlogger.followers)
+                    {
+                        Console.WriteLine($"*  {followerName}");
+                    }
+                }
+                counter++;
+            }
+        }
+    }
+}
---
+++ Sets and Dictionaries Advanced - Exercise/7.TheVlogger/7.TheVlogger/Vlogger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.TheVlogger
+{
+    public class Vlogger
+    {
+        public Vlogger(string name)
+        {
+            Name = name;
+            followers = new SortedSet<string>();
+        }
+        public string Name { get; set; }
+        public SortedSet<string> followers { get; set; }
+        public int followingCount { get; set; }
+    }
+}
